d0505033267e83437b713c5ef48d23efb3b1b418
Melkin Mosquera <melanmosta@gmail.com>
2021-03-02 12:32:26 -0500

    Showing basic article details

---
+++ db/migrate/20210302152428_add_timestamp_to_article.rb
@@ -0,0 +1,6 @@
+class AddTimestampToArticle < ActiveRecord::Migration[6.1]
+  def change
+    add_column :articles, :created_at, :datatime
+    add_column :articles, :updated_at, :datatime
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+## Article operations
+
+Article.create(title: "first article", description: "Description of first article") # make sure Article is capitalized if using this method
+article = Article.new
+article.title = "second article"
+article.description = "description of second article"
+article.save
+article = Article.new(title: "third article", description: "description of third article")
+article.save
+
+article = Article.find(1)
+article.title # to display (get) the title
+article.description # to display (get) the description
+
+Article.first # display the first article in the articles table
+Article.last # display the last article in the articles table
+
+article = Article.find(id of article you want to edit)
+article.title = "new title"
+article.description = "new description"
+article.save
+
+article = Article.find(id of article you want to delete)
+article.destroy
