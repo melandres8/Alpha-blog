a517207d2d21c37d39d7f4be702f35036f41decd
Melkin Mosquera <melanmosta@gmail.com>
2021-03-05 14:15:37 -0500

    Fixing migration issues

--- db/migrate/20210302152428_add_timestamp_to_article.rb
+++
@@ -1,6 +0,0 @@
-class AddTimestampToArticle < ActiveRecord::Migration[6.1]
-  def change
-    add_column :articles, :created_at, :datatime
-    add_column :articles, :updated_at, :datatime
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_02_152428) do
+ActiveRecord::Schema.define(version: 2021_03_05_054256) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
-    t.time "created_at"
-    t.time "updated_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
