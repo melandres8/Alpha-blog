3fc458b3092f1ce4a92ddb31a8e137e57b987cc3
Melkin Mosquera <melanmosta@gmail.com>
2021-03-22 12:47:08 -0500

    Adding users signup system

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
+  has_secure_password
   has_many :articles
   validates :username, presence: true,
                       uniqueness: { case_sensitive: false },
