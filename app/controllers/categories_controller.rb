331a2d22ddf6858c711f257f2e957356e6a0abb0
Melkin Mosquera <melanmosta@gmail.com>
2021-03-23 01:01:59 -0500

    adding categories, playing with many to many association

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,55 @@
+class CategoriesController < ApplicationController
+  before_action :require_admin, except: [:index, :show]
+  before_action :set_category, only: [:show, :edit, :update]
+
+  def index
+    @categories = Category.all.paginate(page: params[:page], per_page: 5)
+  end
+
+  def show
+    @articles = @category.articles.all.paginate(page: params[:page], per_page: 5)
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "Category was successfully created"
+      redirect_to @category
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @category.update(category_params)
+      flash[:notice] = "Category name update successfully"
+      redirect_to @category
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
+  def require_admin
+    if !(logged_in? && current_user.admin?)
+      flash[:alert] = "Only admins can perform that action"
+      redirect_to categories_path
+    end
+  end
+end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,8 @@
 class Article < ApplicationRecord
-  validates :title, presence: true, length: {minimum: 8, maximum: 100}
-  validates :description, presence: true, length: {minimum:20, maximum:300}
+  validates :title, presence: true, length: {minimum: 6, maximum: 100}
+  validates :description, presence: true, length: {minimum:10, maximum:300}
 
   belongs_to :user
+  has_many :article_categories
+  has_many :categories, through: :article_categories
 end
